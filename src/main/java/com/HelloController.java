c2782fed8d15bc6651a86bb18836a519737eb584
Lijiale96 <ljl512@163.com>
2020-03-06 15:26:49 +0800

    Initial commit

---
+++ src/main/java/com/HelloController.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HelloController {
+//    @ResponseBody
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        return "Hello World!";
+//    }
+}
